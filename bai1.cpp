86337b971daa9d6afecdede39ff4f14f0be83c21
TuanCoderIT <133474710+TuanCoderIT@users.noreply.github.com>
2023-10-11 16:22:55 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+double tinhTong(long &n)
+{
+    long double Sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        Sum += 1.0 / (2 * i - 1);
+    }
+    return Sum;
+}
+int main () {
+    long n; 
+    cout <<"Nhap n phan tu: " ;
+    cin >> n;
+    cout <<"Tong = " << tinhTong(n);
+}
+
